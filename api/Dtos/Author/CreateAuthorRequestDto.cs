03b51c3ce395f332a6a3df97d69717a4d4288cee
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2024-07-07 16:14:17 +0300

    Added POST

---
+++ api/Dtos/Author/CreateAuthorRequestDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Author
+{
+    public class CreateAuthorRequestDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+    }
+}
---
+++ api/Dtos/Book/CreateBookRequestDto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Book
+{
+    public class CreateBookRequestDto
+    {
+        [Required]
+        public int? AuthorID {get; set;}
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Genre { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(4, ErrorMessage = "Title cannot be over 4 characters")]
+        public int Year { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        [Required]
+        public string Language { get; set; } = string.Empty;
+    }
+}
